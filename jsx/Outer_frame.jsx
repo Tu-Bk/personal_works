b763bd175847c61599b01c7367b1a6542f69fe0a
Tu-Bk <eric53701654@gmail.com>
2024-09-23 23:33:34 +0800

    0923-React更新

--- jsx/Outer_frame.jsx
+++ jsx/Outer_frame.jsx
@@ -2,62 +2,70 @@ function Outer_frame() {
 
     const cards1 = [
         {
-            id: '1',
+            id: 'card1',
             img: './image/cards_img-4.png',
             area: '埃塞俄比亞',
             Oily: '中等油性',
+            textBox: 'picture_belowp_p1',
             characteristic: '埃塞俄比亞是咖啡的發源地，以多樣的咖啡品種和風味著稱。其咖啡通常帶有豐富的果香和花香，酸度明亮，常有柑橘、莓果等風味。咖啡豆的處理方式以日曬法和水洗法為主。'
         },
         {
-            id: '2',
+            id: 'card2',
             img: './image/cards_img-4.png',
             area: '肯尼亞',
             Oily: '中等油性',
+            textBox: 'picture_belowp_p2',
             characteristic: '肯尼亞的咖啡具有明亮且複雜的酸度，常帶有黑醋栗、柑橘和莓果的風味。因使用水洗法處理，風味乾淨且酸度突出，酒體中等至厚實，適合喜愛強烈酸味的咖啡愛好者。'
         },
         {
-            id: '3',
+            id: 'card3',
             img: './image/cards_img-4.png',
             area: '盧旺達',
             Oily: '中等油性',
+            textBox:'picture_belowp_p4',
             characteristic: '盧旺達的咖啡以其平衡的酸度和甜美的果味聞名，常帶有莓果和紅茶的香氣。採用水洗法處理，保持風味的純淨和乾淨感，風味細膩。'
         },
         {
-            id: '4',
+            id: 'card4',
             img: './image/cards_img-4.png',
             area: '剛果',
             Oily: '中等油性',
+            textBox:'picture_belowp_p5',
             characteristic: '剛果咖啡風味豐富，常帶有莓果、花香和香料的風味。水洗法常用於此區域，確保風味乾淨，同時也有日曬法，使豆子的甜美和果香更為濃郁。'
         },
     ]
 
     const cards2 = [
         {
-            id: '5',
+            id: 'card5',
             img: './image/cards_img-4.png',
             area: '印度',
             Oily: '低油性',
+            textBox:'picture_belowp_p6',
             characteristic: '印度的咖啡豆特別以季風處理法著稱，這使得豆子風味濃郁、酸度低、帶有土味和香料的特徵。印度的阿拉比卡豆和羅布斯塔豆在全球市場上都廣受歡迎，尤其是低酸度的咖啡適合濃縮咖啡愛好者。'
         },
         {
-            id: '6',
+            id: 'card6',
             img: './image/cards_img-4.png',
             area: '印尼',
             Oily: '高油性',
+            textBox:'picture_belowp_p7',
             characteristic: '印尼的咖啡，尤其是蘇門答臘的咖啡，因濕刨法處理而具有厚重的酒體和低酸度，帶有泥土、煙草和巧克力的風味。這種咖啡的油性較高，適合製作濃縮咖啡和深焙咖啡。'
         },
         {
-            id: '7',
+            id: 'card7',
             img: './image/cards_img-4.png',
             area: '越南',
             Oily: '低油性',
+            textBox:'picture_belowp_p8',
             characteristic: '越南主要生產羅布斯塔豆，以其強烈的苦味和厚重的口感聞名。日曬法使羅布斯塔豆的苦味更為突出，油性較低，常用於製作速溶咖啡和咖啡混合物。'
         },
         {
-            id: '8',
+            id: 'card8',
             img: './image/cards_img-4.png',
             area: '巴西',
             Oily: '中等油性',
+            textBox:'picture_below8',
             characteristic: '巴西的咖啡豆具有堅果、巧克力風味，酸度較低，甜度高。日曬法使豆子具有濃厚的口感和豐富的甜味，而蜜處理法則增加了果香。巴西豆適合用於濃縮咖啡和牛奶咖啡。'
         },
     ]
@@ -72,7 +80,7 @@ function Outer_frame() {
                         <img src="./image/card_map.svg" alt="" />
                     </div>
                     <div id="card_map_story">
-                        <img src="./image/Text_carousel_area_img.svg" alt=""  />
+                        <img src="./image/Text_carousel_area_img.svg" alt="" />
                         <marquee color="gray" hspace="1000" scrollamount="10">
                             <p>每個地區的咖啡豆味道都不同，泡出來的味道也不同，靜靜的享受味道也是一種享受。</p>
                         </marquee>
@@ -143,7 +151,7 @@ function Outer_frame() {
                         </div>
                     </div> */}
                     {cards1.map((item) => {
-                        return (<div className="card1 smoove" id="card1" data-move-x="-60%" data-rotate-y="190deg" key={item.id}>
+                        return (<div className="card1 smoove" id={item.id} data-move-x="-60%" data-rotate-y="190deg" key={item.id}>
                             <img src={item.img} alt="" />
                             <div className="card_box_text">
                                 <p>{item.area}</p>
@@ -151,10 +159,11 @@ function Outer_frame() {
                                     <p>{item.Oily}</p>
                                 </div>
                             </div>
-                            <div className="picture_below" id="picture_below1">
+                            <div className="picture_below" id="picture_below1"
+                             onClick={(e) => e.target.style.display = 'flex' ? (e.target.style.display = 'none',console.log(item.textBox)) : e.target.style.display = 'flex'}>
                                 <img src="./image/picture_below.svg" alt="" />
                             </div>
-                            <div className="picture_below  picture_belowp picture_belowp_p" id="picture_belowp_p1">
+                            <div className="picture_below  picture_belowp picture_belowp_p" id={item.textBox}>
                                 <p>{item.characteristic}</p>
                             </div>
                         </div>)
